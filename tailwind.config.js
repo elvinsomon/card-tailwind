bcafe6ec3f9459ef04920e049def42cf9b81e344
elvinsomon <elvinsomon2001@gmail.com>
2020-07-25 16:44:43 -0400

    Primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	purge: [],
+	theme: {
+		fontFamily: {
+			display: ['Playfair Display'],
+		},
+		extend: {
+			colors: {
+				danger: '#c70039',
+				info: {
+					100: '#e4e3e3',
+					200: '#84a9ac',
+					300: '#3b6978',
+					400: '#204051',
+				},
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
+};
